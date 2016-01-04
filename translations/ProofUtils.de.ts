b87a3c4a23e89d5b831bbad3c73b3c215a8c0557
Aliya Iskhakova <aliya.iskhakova@opensoftdev.ru>
2016-01-04 17:44:06 -0500

    add German translation

---
+++ translations/ProofUtils.de.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../powermanager.cpp" line="81"/>
+        <location filename="../powermanager.cpp" line="94"/>
+        <source>No answer from OS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="104"/>
+        <source>User not in sudoers list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="109"/>
+        <source>Incorrect password</source>
+        <translation>Falsches Passwort</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="116"/>
+        <location filename="../powermanager.cpp" line="119"/>
+        <source>Unknown error</source>
+        <translation>Unbekannter Fehler</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="136"/>
+        <source>Can&apos;t restart application</source>
+        <translation>Anwendung kann nicht neu gestartet werden.</translation>
+    </message>
+</context>
+</TS>
