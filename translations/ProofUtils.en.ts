b9ad581b3cd25d182b6a76b6fa3eff5e059be5bb
Denis Kormalev <denis.kormalev@opensoftdev.com>
2016-04-08 15:23:35 +0300

    English ts files added (#966)

---
+++ translations/ProofUtils.en.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../powermanager.cpp" line="77"/>
+        <source>This action is not supported for current platform</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="114"/>
+        <source>Incorrect password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="120"/>
+        <source>Something went wrong during shutdown. Please, contact IT department.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
