a6b1ed2b88e12ae48f533df3f11806d0185dac0d
Aleksey Nikolaev <aleksey.nikolaev@opensoftdev.ru>
2018-11-14 20:23:59 +0300

    Refactoring. Added Feature Summary

--- cmake/FindQRencode.cmake
+++ cmake/FindQRencode.cmake
@@ -1,4 +1,4 @@
-find_path(QRencode_INCLUDE_DIR
+find_path(QRencode_INCLUDE_DIRS
     NAMES "qrencode.h"
     DOC "QRencode include directory"
 )
@@ -11,46 +11,44 @@ mark_as_advanced(QRencode_LIBRARY_RELEASE QRencode_LIBRARY_DEBUG)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(QRencode
-    REQUIRED_VARS QRencode_INCLUDE_DIR QRencode_LIBRARY
+    REQUIRED_VARS QRencode_INCLUDE_DIRS QRencode_LIBRARY
 )
-mark_as_advanced(QRencode_INCLUDE_DIR QRencode_LIBRARY)
 
 if(NOT QRencode_FOUND)
     return()
 endif()
 
-set(QRencode_INCLUDE_DIRS "${QRencode_INCLUDE_DIR}")
 set(QRencode_LIBRARIES "${QRencode_LIBRARY}")
 if(NOT TARGET QRencode::QRencode)
     add_library(QRencode::QRencode INTERFACE IMPORTED)
 endif()
-set_property(TARGET QRencode::QRencode PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${QRencode_INCLUDE_DIRS}")
-set_property(TARGET QRencode::QRencode PROPERTY INTERFACE_LINK_LIBRARIES "${QRencode_LIBRARIES}")
-
-set(QRencode_INCLUDE_DIRS ${QRencode_INCLUDE_DIR})
-if(NOT QRencode_LIBRARIES)
-    set(QRencode_LIBRARIES ${QRencode_LIBRARY})
-endif()
 
 if(TARGET QRencode::QRencode)
-    return()
+    set_property(TARGET QRencode::QRencode PROPERTY INTERFACE_LINK_LIBRARIES "${QRencode_LIBRARIES}")
+    set_property(TARGET QRencode::QRencode PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${QRencode_INCLUDE_DIRS}")
+else()
+    add_library(QRencode::QRencode UNKNOWN IMPORTED)
+    set_target_properties(QRencode::QRencode PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${QRencode_INCLUDE_DIRS}"
+        IMPORTED_LOCATION "${QRencode_LIBRARY}")
+
+    if(QRencode_LIBRARY_RELEASE)
+        set_property(TARGET QRencode::QRencode
+            APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(QRencode::QRencode
+            PROPERTIES IMPORTED_LOCATION_RELEASE "${QRencode_LIBRARY_RELEASE}")
+    endif()
+
+    if(QRencode_LIBRARY_DEBUG)
+        set_property(TARGET QRencode::QRencode
+            APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(QRencode::QRencode
+            PROPERTIES IMPORTED_LOCATION_DEBUG "${QRencode_LIBRARY_DEBUG}")
+    endif()
 endif()
 
-add_library(QRencode::QRencode UNKNOWN IMPORTED)
-set_target_properties(QRencode::QRencode PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${QRencode_INCLUDE_DIRS}"
-    IMPORTED_LOCATION "${QRencode_LIBRARY}")
-
-if(QRencode_LIBRARY_RELEASE)
-    set_property(TARGET QRencode::QRencode
-        APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-    set_target_properties(QRencode::QRencode
-        PROPERTIES IMPORTED_LOCATION_RELEASE "${QRencode_LIBRARY_RELEASE}")
-endif()
-
-if(QRencode_LIBRARY_DEBUG)
-    set_property(TARGET QRencode::QRencode
-        APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-    set_target_properties(QRencode::QRencode
-        PROPERTIES IMPORTED_LOCATION_DEBUG "${QRencode_LIBRARY_DEBUG}")
-endif()
+include(FeatureSummary)
+set_package_properties(QRencode PROPERTIES
+    URL "https://fukuchi.org/works/qrencode/"
+    DESCRIPTION "The QRencode library"
+)
