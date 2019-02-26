518484220470a399fc8d837171ac2013cc6e04ea
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-02-25 16:06:06 -0800

    Asynqro usage

--- include/proofutils/lprprinter.h
+++ include/proofutils/lprprinter.h
@@ -25,7 +25,7 @@
 #ifndef PROOF_UTILS_LPRPRINTER_H
 #define PROOF_UTILS_LPRPRINTER_H
 
-#include "proofseed/future.h"
+#include "proofseed/asynqro_extra.h"
 
 #include "proofcore/proofobject.h"
 
@@ -42,9 +42,9 @@ public:
     explicit LprPrinter(const QString &printerHost, const QString &printerName, bool strictPrinterCheck = false,
                         QObject *parent = nullptr);
 
-    FutureSP<bool> printRawData(const QByteArray &data, bool ignorePrinterState = false) const;
-    FutureSP<bool> printFile(const QString &fileName, unsigned int quantity = 1, bool ignorePrinterState = false) const;
-    FutureSP<bool> printerIsReady() const;
+    Future<bool> printRawData(const QByteArray &data, bool ignorePrinterState = false) const;
+    Future<bool> printFile(const QString &fileName, unsigned int quantity = 1, bool ignorePrinterState = false) const;
+    Future<bool> printerIsReady() const;
 };
 } // namespace Hardware
 } // namespace Proof
