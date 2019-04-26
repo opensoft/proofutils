0ac14dc285d01d8f042bf5c4dda3e7785ddba8f3
Dmitry Nesterenok <nesterenokda@gmail.com>
2019-04-26 11:50:50 +0400

    Add empty Russian translation

--- translations/ProofUtils.de.ts
+++ translations/ProofUtils.de.ts
@@ -4,81 +4,10 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../powermanager.cpp" line="81"/>
-        <source>This action is not supported for current platform</source>
-        <translation>Dieser Funktion wird bei deser Version nicht unterstützt</translation>
-    </message>
-    <message>
-        <location filename="../powermanager.cpp" line="118"/>
-        <source>Incorrect password</source>
-        <translation>Falsches Passwort</translation>
-    </message>
-    <message>
-        <location filename="../powermanager.cpp" line="124"/>
-        <source>Something went wrong during shutdown. Please, contact IT department.</source>
-        <translation>Beim Herunterfahren ist ein Fehler aufgetreten. Bitte wenden Sie sich an die IT-Abteilung.</translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="338"/>
-        <location filename="../networkconfigurationmanager.cpp" line="957"/>
-        <source>VPN can&apos;t be started</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="341"/>
-        <location filename="../networkconfigurationmanager.cpp" line="982"/>
-        <source>VPN can&apos;t be stopped</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="466"/>
-        <location filename="../networkconfigurationmanager.cpp" line="504"/>
-        <source>Can&apos;t read network configuration</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="626"/>
-        <location filename="../networkconfigurationmanager.cpp" line="640"/>
-        <location filename="../networkconfigurationmanager.cpp" line="650"/>
-        <location filename="../networkconfigurationmanager.cpp" line="709"/>
-        <location filename="../networkconfigurationmanager.cpp" line="720"/>
-        <location filename="../networkconfigurationmanager.cpp" line="725"/>
-        <location filename="../networkconfigurationmanager.cpp" line="736"/>
-        <location filename="../networkconfigurationmanager.cpp" line="741"/>
-        <location filename="../networkconfigurationmanager.cpp" line="792"/>
-        <source>Can&apos;t write network configuration</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="863"/>
-        <source>Proxy configuration can&apos;t be written. Permission denied</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="898"/>
-        <source>VPN configuration can&apos;t be opened</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="903"/>
-        <source>VPN configuration not found</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="917"/>
-        <location filename="../networkconfigurationmanager.cpp" line="930"/>
-        <location filename="../networkconfigurationmanager.cpp" line="935"/>
-        <source>Can&apos;t write VPN configuration</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="962"/>
-        <source>VPN can&apos;t be started. Password is wrong</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../networkconfigurationmanager.cpp" line="987"/>
-        <source>VPN can&apos;t be stopped. Password is wrong</source>
+        <location filename="../src/proofutils/lprprinter.cpp" line="271"/>
+        <location filename="../src/proofutils/lprprinter.cpp" line="284"/>
+        <source>Printer 
+%1@%2 is not ready.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
